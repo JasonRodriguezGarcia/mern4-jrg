e1a87960417a85d22ade2c4eef14a0bc0b2b6c2d
Jason RG <jasonrodriguezempleo@gmail.com>
2025-05-23 15:04:29 +0200

    test

--- orm/db_pg.py
+++ orm/db_pg.py
@@ -11,6 +11,7 @@ DB_NAME = 'demo' # accediend a bbdd demo o bien postgres
 engine = create_engine(f'postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
 
 # Create a configured "Session" class
+# Create a configured "Session" class
 SessionLocal = sessionmaker(bind=engine)
 
 def get_session():
