fb1c8e0aeab9536a104c31518fd2949dcfc98760
JasonRodriguezGarcia <jasonrodriguezempleo.gmail.com>
2025-06-06 10:58:30 +0200

    05Junio updated3

--- express-mongodb/routes/seguridad.js
+++ express-mongodb/routes/seguridad.js
@@ -13,48 +13,47 @@ router.use((req, res, next)=> {
     const role = req.headers[`x-user-role`];
     const userId = req.headers['x-user-id']
 
-    req.role = role;        //aunque esta disponble en la funcion centralizamos aqui
+    req.role = role;        //aunque esta disponble en la funcion centralizamos aqui, disponibles globalmente
     req.userId = userId;
     
     // if (!apiKey || !ALLOWED_API_KEYS.includes(apiKey)) {
         //     return res.status(403).json({message: "Forbidden Access"})
         // }
         
-        req.role = role
-        
-        next();
-    });
+    req.role = role
     
-    // GET /api/v1/seguridad
-    router.get('/', authorise('read:any'), async (req, res) => {
-        
-        try {
-            console.log("Here express");
-            res.json({message: "exito", role: req.role});
-            
-        } catch (error) {
-            console.error("Error fetching seguridad:", error);
-            res.status(500).json({ error: 'Failed to fetch seguridad' });
-        }
-    });
+    next();
+});
     
-    // GET /api/v1/seguridad/23490872390472987 id de MongoDB
-    router.get('/:id', authoriseOwnership(), async (req, res) => {
-        const db = req.app.locals.db
-        try {
-
-
-            const user = await db.collection("usuarios").findOne({"_id": new ObjectId(req.userId)})
-            console.log("imprimo user: ", user)
-            res.json({message: "exito", role: req.role});
-
-        } catch (error) {
-            console.error("Error fetching seguridad:", error);
-            res.status(500).json({ error: 'Failed to fetch seguridad' });
-        }
+// GET /api/v1/seguridad
+router.get('/', authorise('read:any'), async (req, res) => {
+    
+    try {
+        console.log("Here express");
+        res.json({message: "exito", role: req.role});
+        
+    } catch (error) {
+        console.error("Error fetching seguridad:", error);
+        res.status(500).json({ error: 'Failed to fetch seguridad' });
     }
-);
+});
 
+// GET /api/v1/seguridad/23490872390472987 id de MongoDB
+router.get('/:id', authoriseOwnership(), async (req, res) => {
+    const db = req.app.locals.db
+    try {
 
+        // new ObjetId es para versiones MongoDB inferiores a la 5, AUNQUE POR AHORA FUNCIONAN
+        // const user = await db.collection("usuarios").findOne({"_id": new ObjectId(req.userId)})
+        // para versiones MongoDB >= 5 podemos poner
+        const user = await db.collection("usuarios").findOne({_id: ObjectId.createFromHexString(req.userId)})
+        console.log("imprimo user: ", user)
+        res.json({message: "exito", role: req.role, });
+
+    } catch (error) {
+        console.error("Error fetching seguridad:", error);
+        res.status(500).json({ error: 'Failed to fetch seguridad' });
+    }
+});
 
 export default router;
