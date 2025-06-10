96b08d3926632deafa5d7df6f65800d1aaf47264
JasonRodriguezGarcia <jasonrodriguezempleo.gmail.com>
2025-06-10 23:23:44 +0200

    10Mayo updated

--- vite-frontend/src/components/MeGusta.jsx
+++ vite-frontend/src/components/MeGusta.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+
 
 const MeGusta = () => {
 
@@ -20,13 +22,13 @@ const MeGusta = () => {
             descripcion: "Comprar pistachos a la tarde"
         }
     ])
-    
+    const [likes, setLikes] = useState({});
     
     const handleMeGusta = async (meGustaID) => {
         try {
             const query = `
-                mutation AddPostLike ($input: PostLikeInput!) {
-                    addPost(input: $input)
+                mutation addPostLike ($input: PostLikeInput!) {
+                    addPostLike(input: $input)
                 }
                 `;
             const variables = {
@@ -41,7 +43,7 @@ const MeGusta = () => {
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ query , variables
 
-                    // query: 
+                    // query:  // otra forma un tanto diferente
                     //     `mutation ($id: Int) {
                     //         addPostLike (input: {
                     //             id: $id
@@ -58,19 +60,28 @@ const MeGusta = () => {
 
                 }),
         });
-            const json = await response.json();
-            if (json.errors) {
-                setError(json.errors[0].message);
-            } else {
-                if (action == 0)
-                    setRecetas(json.data.recetas);
-                else if (action == 1)
-                    fetchRecetas(0)
-            }
+            // const json = await response.json();
+            // if (json.errors) {
+            //     setError(json.errors[0].message);
+            // } else {
+            //     if (action == 0)
+            //         setRecetas(json.data.recetas);
+            //     else if (action == 1)
+            //         fetchRecetas(0)
+            // }
         } catch (err) {
             // setError('Error fetching data');
         }
 
+    }
+
+    const toggleLike = (id) => {
+        const newLikes = {
+            ...likes, [id]: !likes[id]
+        }
+        setLikes(newLikes)
+        console.log("imprimo like: ", newLikes)
+        console.log("newLikes.id: ", newLikes[id])
     }
 
     return (
@@ -79,11 +90,23 @@ const MeGusta = () => {
             <ul style={{textAlign: "left"}}>
                 {posts.map((post, index)=> (
                     <li key={index}>
-                        <button type="button" style={{
+                        {/* <button type="button" style={{
                             backgroundColor: "blue", color: "white", margin: "10px"
                             }}
                             onClick={()=> handleMeGusta(index)}
-                        >Me Gusta</button>
+                        >Me Gusta</button> */}
+                        <button
+                            onClick={() => toggleLike(post.id)}
+                            style={{ background: 'none', border: 'none', cursor: 'pointer' }}
+                        >
+                            <ThumbUpIcon
+                            style={{
+                                color: likes[post.id] ? 'blue' : 'gray',
+                                fontSize: 30,
+                            }}
+                            />
+                        </button>
+
                         {post.id} -- {post.descripcion} 
                     </li>
                 ))}
